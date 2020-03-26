3f0eac4119fba44bf04fb07f0e2ca30c7343dc2c
bergmaner <bergkacper@gmail.com>
2020-03-26 15:21:13 +0100

    checkWin

--- src/Components/Game/index.js
+++ src/Components/Game/index.js
@@ -16,13 +16,36 @@ class Game extends Component {
             ]
         }
     }
-    
-    
+
+    checkWinner = (squares) =>
+    {
+        const possibilities =
+        [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+
+        for(let i=0 ; i < possibilities.length ; i++)
+        {
+            const [x,y,z] = possibilities[i];
+            if(squares[x] && squares[x] === squares[y] && squares[y] === squares[z]) return squares[x];
+            
+        }
+        return null;
+
+    }
     onClick = (n) => {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
-        
+        const winner = this.checkWinner(squares);
+       if(winner || squares[n]) return ;
         squares[n] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat({
@@ -38,13 +61,19 @@ class Game extends Component {
     {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
-  return (
+        const squares = current.squares.slice();
+        const winner = this.checkWinner(squares);
+        let status;
+         (winner) ? status = `Winner is ${winner}`  : status = `Winner is ` + this.state.xIsNext ? 'X' : 'O';
+  
+         return (
     <div className="game">
     <div className = "game-board">
     <Board 
     squares={current.squares}
     onClick = {(n) => this.onClick(n)}
     ></Board>
+    <div>{status}</div>
     </div>
     </div>
   );         
